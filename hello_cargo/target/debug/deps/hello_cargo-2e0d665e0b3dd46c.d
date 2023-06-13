49208c3d82e4f6c65febf9c0d11da1d513d54537
murim77 <murim77@naver.com>
2023-06-13 12:28:14 +0900

    new files

---
+++ hello_cargo/target/debug/deps/hello_cargo-2e0d665e0b3dd46c.d
@@ -0,0 +1,5 @@
+/home/maru/work/rustsrc/ruststudy/hello_cargo/target/debug/deps/hello_cargo-2e0d665e0b3dd46c: src/main.rs
+
+/home/maru/work/rustsrc/ruststudy/hello_cargo/target/debug/deps/hello_cargo-2e0d665e0b3dd46c.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/maru/work/rustsrc/ruststudy/hello_cargo/target/debug/hello_cargo: /home/maru/work/rustsrc/ruststudy/hello_cargo/src/main.rs
