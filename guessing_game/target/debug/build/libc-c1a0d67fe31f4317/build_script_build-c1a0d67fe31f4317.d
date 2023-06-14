c78490f45d310b41c1d399da018090a5a6f7f2bd
murim77 <murim77@naver.com>
2023-06-14 14:12:11 +0900

    dependencies add

---
+++ guessing_game/target/debug/build/libc-c1a0d67fe31f4317/build_script_build-c1a0d67fe31f4317.d
@@ -0,0 +1,5 @@
+/home/maru/work/rustsrc/ruststudy/guessing_game/target/debug/build/libc-c1a0d67fe31f4317/build_script_build-c1a0d67fe31f4317: /home/maru/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.146/build.rs
+
+/home/maru/work/rustsrc/ruststudy/guessing_game/target/debug/build/libc-c1a0d67fe31f4317/build_script_build-c1a0d67fe31f4317.d: /home/maru/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.146/build.rs
+
+/home/maru/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.146/build.rs:
